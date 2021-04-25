ceb0059cea567b20caa2a00fd9c67804b7cb5bf0
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-04-25 15:35:04 -0500

    Activity 1

---
+++ Activity 1/FirstMVCWebApp/FirstWebApp/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LogMeIn(UserModel usermodel)
+        {
+            if(usermodel.Username == "NKumar1" && usermodel.Password == "12345")
+            {
+                return View("LoginSuccess", usermodel);
+            }
+            else
+            {
+                return View("LoginFail", usermodel);
+            }
+            
+        }
+    }
+}
---
+++ Activity 1/FirstMVCWebApp/FirstWebApp/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Test/Play
+        public string Play()
+        {
+            // Return a string as a View
+            return @"<b>Hello World as a string from Play</b>";
+        }
+
+        //GET: /Test/TestView
+        public IActionResult TestView()
+        {
+            //Return "TestView.cshtml"
+            return View();
+        }
+    }
+}
